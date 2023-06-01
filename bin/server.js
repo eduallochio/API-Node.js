838672b3bc5207ac4278b4a58e508ff55d3e62c9
Eduardo Jonais Silva Allochio <eduallochio2@outlook.com>
2023-06-01 20:55:35 -0300

    incluido rotas

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ const debug = require('debug')('omega:server');
 const http = require('http');
 
 const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port)
+app.set('port', port);
 
 const server = http.createServer(app);
 
@@ -13,45 +13,46 @@ server.on('listening', onListening);
 console.log('API rodando na porta ' + port);
 
 function normalizePort(val) {
-    const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    const bind = typeof port === 'string' ?
-        'Pipe' + port :
-        'Port' + port;
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + 'requires elevated privileges');
-            precess.exit(1);
-        case 'EADDRINUSE':
-            console.error(bind + 'is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
-function onListening(){
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-    ? 'pipe' + addr
-    : 'port' + addr.port;
-    debug('Listening on ' + bind);
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
 }
